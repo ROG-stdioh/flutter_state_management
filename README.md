dd879171b569a04f64218d69f8416381ba68d1fc
ROG-stdioh <90563301+ROG-stdioh@users.noreply.github.com>
2022-09-09 21:25:13 +0530

    adding functions and readme update

--- README.md
+++ README.md
@@ -1,16 +1,44 @@
-# flutter_state_management
+## **FLUTTER STATE MANAGEMENT - The Basics**
 
-A new Flutter project.
+### In this branch of Flutter State Management, I'll be focusing on the basics.
+
+---
+
+## What is state management?
+
+- State management, in a broder spectrum, is managing dependencies in an organized and orchestrated way
+- In a simpler world, state management is how one can glue together various layers of an application.
+- Usually, it includes going through the service layer, business layer and then to the UI layer; it's how one glues together these three layers.
+- One may have more (or less) layers in the application that may or may not depend on the states (or data) coming from the backend. This makes the State Management's definition _"fluid"_ and makes it the user's choice how he/she wishes to interpret.
+
+---
+
+## About this branch
+
+- In this branch, I explored some of the _"built-in"_ ways that flutter allows us to manage the state of the application _"internally"_.
+- In this, we'll be working on a very simple appliication in which we'll display the list of contacts defined by their name (FName and LName) _i.e._ a name property that we're going to define on the class called **_contact_** on a **_ListView_** and another view for adding new contacts which will be displayed on the List and remove contacts from the list using **_dismissal_** in Flutter.
+
+---
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+Let's setup the project first.
 
-A few resources to get you started if this is your first Flutter project:
+First, in the `main.dart` file, make a `stl` widget that returns a `material app` which has `HomePage.dart` (which returns an empty scaffold) as the home. After this, we'll define all the required classes etc. See the files.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+- Note: These are some resources to get you started if this is your first Flutter project:
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+  - [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+  - [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+---
+
+## **About me**
+
+I am a Third Year, Computer Engineering student at Army Institute of Technology, Pune who loves UI/UX, Flutter, Digital Art and a good slice of Pizza 😊.
+
+![](https://media.giphy.com/media/111ebonMs90YLu/giphy.gif)
+
+Socials: [LinkedIn](https://www.linkedin.com/in/surya2807/) and [Instagram](instagram.com/a.k.a._surya/)
+
+---
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_element, unused_field
 
 import 'package:flutter/material.dart';
 
@@ -7,6 +7,64 @@ class HomePage extends StatefulWidget {
 
   @override
   State<HomePage> createState() => _HomePageState();
+}
+
+/// +-----------------------------------------------------------------------------------------------+
+/// | We'll define the class "Contact" that'll be defined by a final string "name"                  |
+/// | VSCode will make a constructor for this which will be a "constant" and a "required" parameter |
+/// +-----------------------------------------------------------------------------------------------+
+class Contact {
+  final String name;
+
+  // Constructor
+  const Contact({
+    required this.name,
+  });
+}
+
+/// +-----------------------------------------------------------------------------------------------------------------------------+
+/// | Another thing that we need for the state management we're going to use in this branch is a class called "Contact Book"      |
+/// | , a singleton class which will have only one instance available in the entire application and "Contact Book" will manage    |
+/// | state of the contacts so that one can add or remove contacts, fetch contacts from an index and get lengths of all the       |
+/// | available contacts in this class.                                                                                           |
+/// | We'll begin with a very simple "VANILLA" contact book class which won't have state management as such but, it'll have a way |
+/// | of hiding its internals by not exposing, for instance, the entire list to the outside world.                                |
+/// | After this, we'll create a singleton for "ContactBook". We just want one instance of this class in the entire appplication. |
+/// | => Now, we'll create a "private constructor" and we call a "shared instance" and we'll create a "static final" and call     |
+/// |    we'll call it ContactBook. It'll return "ContactBook" and we say it's "shared"  (*THIS IS ALSO PRIVATE* as it is being   |
+/// |    used privately).                                                                                                         |
+/// | This is the pattern of creating singletons in dart.                                                                         |
+/// | => Then, we'll create a "FACTORY" of ContactBook that reruns the shared static final.                                       |
+/// | => Now, we'll create a storage for all the contacts. For this, we'll create an array. And since we are going to be using a  |
+/// |    ListView in our application, we basically need to expose the contacts that this ContactBook is holding on to. So, for    |
+/// |    the ListView Builder, we'll have to expose the item count to the application.                                            |
+/// | => After this, we'll make a simple "add" function that takes the contact and adds it to the contact list.                   |
+/// | => After the "add" function, we'll make a simple "remove" function that removes the contact from the contact list.          |
+/// | => We'll also have to have a "retrieve" function that can return a contact using its index. (NOTE: we are not going to      |
+/// |    expose the contact as subscript using the function, we're actually going to make return an optional contact and it'll    |
+/// |    return the contact's index is actually in range). This is an error function so. it doesn't require any return statement. |
+/// |    Then, there is a ternary operator and it's saying that "SHOULD the length of the contacts be MORE than the index         |
+/// |    provided, then we actually return at that index otherwise we return NULL".                                               |
+/// +-----------------------------------------------------------------------------------------------------------------------------+
+class ContactBook {
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+
+  final List<Contact> _contacts = [];
+
+  int get length => _contacts.length;
+
+  void add({required Contact contact}) {
+    _contacts.add(contact);
+  }
+
+  void remove({required Contact contact}) {
+    _contacts.remove(contact);
+  }
+
+  Contact? contact({required int atIndex}) =>
+      _contacts.length > atIndex ? _contacts[atIndex] : null;
 }
 
 class _HomePageState extends State<HomePage> {
